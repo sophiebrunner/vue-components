49c13366a726e39b171bb5989a1541ea43a8ed80
Sophie Martina Margarete Brunner <95913912+sophiebrunner@users.noreply.github.com>
2022-08-30 15:27:19 +0200

    add, register and use MyFirstComponent and import reset.css

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Welcome to Your Vue.js App" />
+  <MyFirstComponent />
 </template>
 
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
-
+import MyFirstComponent from "./components/MyFirstComponent.vue";
 export default {
   name: "App",
   components: {
     HelloWorld,
+    MyFirstComponent,
   },
 };
 </script>
