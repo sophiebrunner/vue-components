1c9bde7fa10acab3794c5b6ac8b2ec95d2d23901
Sophie Martina Margarete Brunner <95913912+sophiebrunner@users.noreply.github.com>
2022-08-31 10:51:02 +0200

    feat: add props

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Welcome to Your Vue.js App" />
-  <MyFirstComponent />
+  <MyFirstComponent :headline="headline" type="" />
+  <MyFirstComponent :headline="headline" type="red" />
+  <MyFirstComponent :headline="headline" type="blue" />
+  <MyFirstComponent :headline="headline" type="purple" />
 </template>
 
 <script>
@@ -9,6 +12,11 @@ import HelloWorld from "./components/HelloWorld.vue";
 import MyFirstComponent from "./components/MyFirstComponent.vue";
 export default {
   name: "App",
+  data() {
+    return {
+      headline: "My Headline Hi World",
+    };
+  },
   components: {
     HelloWorld,
     MyFirstComponent,
--- src/components/MyFirstComponent.vue
+++ src/components/MyFirstComponent.vue
@@ -1,7 +1,39 @@
-<template>Hallo Welt</template>
+<template>
+  <h1 :class="cssTypeClass">{{ headline }}</h1>
+</template>
 
 <script>
 export default {
   name: "MyFirstComponent",
+  props: {
+    headline: String,
+    type: {
+      type: String,
+      default: "green",
+    },
+  },
+  computed: {
+    cssTypeClass() {
+      return `text-${this.type}`;
+    },
+  },
 };
 </script>
+
+<style scoped>
+.text-green {
+  color: green;
+}
+
+.text-red {
+  color: red;
+}
+
+.text-blue {
+  color: blue;
+}
+
+.text-purple {
+  color: purple;
+}
+</style>
