c2fe1e7302e8e74c066464e475ecf5fedd3502a4
Sophie Martina Margarete Brunner <95913912+sophiebrunner@users.noreply.github.com>
2022-09-06 18:11:40 +0200

    feat: add router-plugin

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,30 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
-  <MyFirstComponent :headline="headline" type="" />
-  <MyFirstComponent :headline="headline" type="red" />
-  <MyFirstComponent :headline="headline" type="blue" />
-  <MyFirstComponent :headline="headline" type="purple" />
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav>
+  <router-view />
 </template>
 
-<script>
-import HelloWorld from "./components/HelloWorld.vue";
-import MyFirstComponent from "./components/MyFirstComponent.vue";
-export default {
-  name: "App",
-  data() {
-    return {
-      headline: "My Headline Hi World",
-    };
-  },
-  components: {
-    HelloWorld,
-    MyFirstComponent,
-  },
-};
-</script>
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,25 @@
+<template>
+  <img alt="Vue logo" src="@/assets/logo.png" />
+  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <MyFirstComponent :headline="headline" type="" />
+  <MyFirstComponent :headline="headline" type="red" />
+  <MyFirstComponent :headline="headline" type="blue" />
+  <MyFirstComponent :headline="headline" type="purple" />
+</template>
+
+<script>
+import HelloWorld from "@/components/HelloWorld.vue";
+import MyFirstComponent from "@/components/MyFirstComponent.vue";
+export default {
+  name: "App",
+  data() {
+    return {
+      headline: "My Headline Hi World",
+    };
+  },
+  components: {
+    HelloWorld,
+    MyFirstComponent,
+  },
+};
+</script>
